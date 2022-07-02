b7ff82bbe0364299f2207dbbf40672b6d57edac7
a.alghamdi <a.alghamdi@juleb.com>
2022-07-02 15:14:00 +0300

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (
+      '<h1>App is healthy</h1>' + 
+      '<p>Deployed Successfully By : <b>Aziz</b> </p>' +
+      '<p>Version : <b>1.0</b></p>'
+      );
   }
 }
