4d514784aab0c406025a27d8eda41acbdf90df63
Abdulaziz Alghamdi <a.alghamdi@juleb.com>
2022-07-06 09:59:59 +0300

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -12,7 +12,7 @@ export class AppService {
     return (
       '<h1>Hello !</h1>' +
       '<p>Deployed Successfully By : <b>Aziz</b> </p>' +
-      '<p>Version : <b>1.0.0</b></p>'
+      '<p>Version : <b>1.0.1</b></p>'
     );
   }
 }
